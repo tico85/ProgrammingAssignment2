81a1d113a4f45a89b2a6902950f95e4bf22e8f92
tico85 <tcossette@gmail.com>
2017-02-28 12:44:04 -0800

    Add Existing File

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions combined will return the inverse of a solvable matrix, returning
+## the value from the cache if it is availble there. 
 
-## Write a short comment describing this function
+## This function clears the existing cache and sets a list of functions that will be used
+## to resolve the CacheSOlve funtion
 
 makeCacheMatrix <- function(x = matrix()) {
 
-}
+    imatcache<<- NULL
+    
+    get <- function() x 
+    setimatrix <- function(imat) imatcache <<- imat 
+    getimatrix <- function() imatcache 
+    list(get = get,
+         setimatrix = setimatrix,
+         getimatrix = getimatrix)
+
+    }
 
 
-## Write a short comment describing this function
+
+## This function returns the inverse of a matrix, returning it from the cache if it is
+## available 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+    
+    imatcache <- x$getimatrix()
+    if(!is.null(imatcache)){
+        message("Inverted Matrix Returned from cache")
+        return(imatcache)
+    }
+    calc<-x$get()
+    traccalc<<-x$get()
+    imat <- solve(calc)
+    x$setimatrix(imat)
+    imat
+    
 }
+
